1fb9aa7abfb83612911b9c509e6b6b571776a868
JS70777 <27jshiu@athenian.org>
2025-02-19 10:40:52 -0800

    working on RandomStringChooser

--- src/RandomChooser/RandomStringChooser.java
+++ src/RandomChooser/RandomStringChooser.java
@@ -1,4 +1,21 @@
 package RandomChooser;
 
 public class RandomStringChooser {
+    private String[] words;
+    
+    public RandomStringChooser(String[] wordArray) {
+        words = wordArray;
+    }
+    
+    public String getNext() {
+        int i = (int) (Math.random() * words.length + 1);
+        String[] newWords = new String[words.length - 1];
+        int k = 0;
+        for(int j = 0; j < newWords.length; j++) {
+            if(j != i) {
+            
+            }
+        }
+        return
+    }
 }
